85f2380e3b73033c51d5cde772ec76dcacb8b65b
Ervin <hamoydoerik@gmail.com>
2021-03-23 20:16:47 +0200

    Initial

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+
+const keys = require('./keys/index');
+
+const app = express();
+
+app.use(express.json({extended: true}));
+
+if (process.env.NODE_ENV === 'production') {
+   app.use('/', express.static(path.join(__dirname, 'client', 'build')));
+
+   app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+   });
+}
+
+const PORT = process.env.PORT || keys.PORT || 5000;
+
+async function start() {
+   try {
+      //  || config.get('mongoURI')
+      await mongoose.connect(process.env.MONGODB_URI || keys.MONGODB_URI, {
+         useNewUrlParser: true,
+         useUnifiedTopology: true,
+         useCreateIndex: true,
+         useFindAndModify: false
+      });
+
+      app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`));
+   } catch (error) {
+      console.log('Server error: ', error.message);
+      process.exit(1);
+   }
+}
+start();
