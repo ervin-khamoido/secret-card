79abdd9029e074d29bf25d5847d2023944def3a3
Ervin <hamoydoerik@gmail.com>
2021-04-28 20:08:31 +0300

    Added an additional check for authorization

--- client/src/pages/CardHistory.js
+++ client/src/pages/CardHistory.js
@@ -3,21 +3,32 @@ import { AuthContext } from "../context/AuthContext";
 import { useHttp } from "../hooks/http.hook";
 import {Loader} from '../components/Loader';
 import { CardsList } from "../components/CardsList";
+import { useHistory } from "react-router";
 
 export const CardHistory = () => {
    const [cards, setCards] = useState([]);
    const {loading, request} = useHttp();
-   const {token} = useContext(AuthContext);
+   const {token, logout} = useContext(AuthContext);
+   const history = useHistory();
 
    const fetchCards = useCallback(async () => {
       try {
          const fetched = await request('/api/history', 'GET', null, {
             Authorization: `Bearer ${token}`
          });
+
+         if (fetched === 401) {
+            logout();
+            history.push('/');
+         }
+
+         if (fetched === 500) {
+            console.log('500 status code');
+         }
          
          setCards(fetched);
       } catch (error) {}
-   }, [token, request])
+   }, [token, request, history, logout])
 
    useEffect(() => {
       fetchCards();
@@ -30,7 +41,6 @@ export const CardHistory = () => {
    return (
       <>
          <h1 style={{textAlign: 'center'}}>History of created cards</h1>
-         
          {!loading && <CardsList cards={cards} />}
       </>
    )
